ab6c9660cb0a95e8f463e022d77c477a51898971
Herve Menard <hmenard@dundee.ac.uk>
2023-06-15 10:56:37 +0100

    Code update for publication

--- CombineData.R
+++
@@ -1,8 +0,0 @@
-
-CardData <- read.csv("CardData.csv", sep=",", header=TRUE)
-names(CardData)[1] <-"SampleName"
-
-CardData <- CardData[!is.na(CardData$Dilution), ]
-
-
-TempDataCard <- full_join(CardData,CombinedData)
